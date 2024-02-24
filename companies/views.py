be781be99b12d2b38a9b6c6e05f233a8f3dc31c8
bleedcloudVIII <yurikaras8@gmail.com>
2024-02-24 17:35:45 +1000

    SHIIIIIIIIIITTTTT

--- companies/views.py
+++ companies/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render, HttpResponse
 
 def index(request):
-    return render(request, 'company_index.html')
+    # Пока не выбраны услуги и т.д.
+    # То берутся первые 10 компаний
+    context = {
+        'companies':[
+            {'name': 'Company1',
+                'spez': 'Parikmaxerskaya',
+                'services': [
+                'strizka', 'okrashivanie',
+            ]},
+            {'name': 'Company2',
+                'spez': 'Banya',
+                'services': [
+                'Banya', 'Sauna', 'Hamam',
+            ]},
+        ]
+    }
+    return render(request, 'company_index.html', context)
 
 def registration(request):
     return render(request, 'company_registration.html')
